049d7520ab5b49374f23918f3f4daddad6769f72
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-24 16:05:18 -0400

    Adding helper functions for drawing lines and squares

--- js/fractals.js
+++ js/fractals.js
@@ -10,3 +10,39 @@ $(function(){
   ctx=c.getContext("2d");
   ctx.fillStyle="444444";
 });
+
+function draw_line(x1, y1, x2, y2){
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
+
+function draw_square(left_x, top_y, dim){
+  var right_x = left_x + dim
+  var bottom_y = top_y + dim
+
+  // Draw top line
+  draw_line(left_x, top_y, right_x, top_y)
+  // Draw right line
+  draw_line(right_x, top_y, right_x, bottom_y)
+  // Draw bottom line
+  draw_line(left_x, bottom_y, right_x, bottom_y)
+  // Draw left line
+  draw_line(left_x, top_y, left_x, bottom_y)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
