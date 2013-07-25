04f2be8fe7ae65195ac9aea8397ceed4f4bdce4e
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-25 00:44:26 -0400

    Drawing square and checking for base case

--- js/fractals.js
+++ js/fractals.js
@@ -18,34 +18,33 @@ function draw_line(x1, y1, x2, y2){
 }
 
 function draw_square(left_x, top_y, dim){
-  var right_x = left_x + dim
-  var bottom_y = top_y + dim
+  var right_x = left_x + dim;
+  var bottom_y = top_y + dim;
 
   // Draw top line
-  draw_line(left_x, top_y, right_x, top_y)
+  draw_line(left_x, top_y, right_x, top_y);
   // Draw right line
-  draw_line(right_x, top_y, right_x, bottom_y)
+  draw_line(right_x, top_y, right_x, bottom_y);
   // Draw bottom line
-  draw_line(left_x, bottom_y, right_x, bottom_y)
+  draw_line(left_x, bottom_y, right_x, bottom_y);
   // Draw left line
-  draw_line(left_x, top_y, left_x, bottom_y)
+  draw_line(left_x, top_y, left_x, bottom_y);
 }
 
 function recursive_square(left_x, top_y, dim){
-  // draw a square according to those dimenstions
+  // draw a square according to those dimensions
+  draw_square(left_x, top_y, dim);
 
   // if our dimension was more than 1
-  // halve the dimensions and
+  if (dim >= BASE_CASE_DIM) {
+    // halve the dimensions and
 
-  // call recursive square function again, with new dimension and same coordinates
+    // call recursive square function again, with new dimension and same coordinates
 
-  // call recursive square function again, with new dimension and coordinates of
-  // right side (old left coordinate plus old dimension) minus new dimension and
-  // bottom side (old top coordinate plus old dimension) minus new dimension
-
-
-
-
+    // call recursive square function again, with new dimension and coordinates of
+    // right side (old left coordinate plus old dimension) minus new dimension and
+    // bottom side (old top coordinate plus old dimension) minus new dimension
+  }
 }
 
 
